620d9a3c4c3d657e2144cfd323ce9455665978b3
gliu <gliu@actuate.com>
2012-10-31 16:47:05 +0800

    local commit 16:46

--- src/demo/Git.java
+++ src/demo/Git.java
@@ -6,7 +6,7 @@ public class Git {
 		System.out.println("master2 change");
 		System.out.println("master2 self content");
 		System.out.println("master2 self content");
-                  System.out.println("write from github");
-
+        System.out.println("write from github");
+        System.out.println("write from local");
 	}
 }
