57d552ac76ff3689ec13c89df5ba7afca87f1ab0
gliu <gliu@actuate.com>
2012-10-31 14:31:31 +0800

    master2 commit 14:30

--- src/demo/Git.java
+++ src/demo/Git.java
@@ -5,5 +5,6 @@ public class Git {
 	public void method() {
 		System.out.println("master2 change");
 		System.out.println("master2 self content");
+		System.out.println("master2 self content");
 	}
 }
